0a9cd43c8523b5628b84bed1c67f08569117309b
최준영 <cjy4780@naver.com>
2025-10-18 21:39:21 +0900

    feat: 테스트케이스 업데이트

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,11 +1,11 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -17,10 +17,82 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 같은_기호로_이루어진_구분자() {
+        assertSimpleTest(() -> {
+            run("/// //\\n1/2//3,4");
+            assertThat(output()).contains("결과 : 10");
+        });
+    }
+
+    @Test
+    void 구분자로_메타문자_사용() {
+        assertSimpleTest(() -> {
+            run("/// < > ( )\\n1/2<3>4(5)6");
+            assertThat(output()).contains("결과 : 21");
+        });
+    }
+
+    @Test
+    void 문자_커스텀_구분자() {
+        assertSimpleTest(() -> {
+            run("//wo te ko\\n2025wo10te14ko8");
+            assertThat(output()).contains("결과 : 2057");
+        });
+    }
+
+    @Test
+    void 커스텀_구분자_사용안함() {
+        assertSimpleTest(() -> {
+            run("1");
+            assertThat(output()).contains("결과 : 1");
+        });
+    }
+
+    @Test
+    void 입력값_공백() {
+        assertSimpleTest(() -> {
+            run("\n");
+            assertThat(output()).contains("결과 : 0");
+        });
+    }
+
+    @Test
+    void 예외_테스트_하이픈() {
         assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
+                assertThatThrownBy(() -> runException("-1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자연속() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2,,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_숫자_구분자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("//1\\n1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_앞뒤_구분자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(",1,2,3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_구분자_아님() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1,2a3"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
