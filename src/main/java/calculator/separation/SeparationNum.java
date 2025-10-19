9ab47f792c6ddd33ee277e3d272f756b25823eca
최준영 <cjy4780@naver.com>
2025-10-20 01:31:39 +0900

    refactor: 직관성을 위한 변수명과 메소드명 수정
    String str -> String numStr
    String s -> String separator

--- src/main/java/calculator/separation/SeparationNum.java
+++ src/main/java/calculator/separation/SeparationNum.java
@@ -1,27 +1,26 @@
 package calculator.separation;
 
 import calculator.exception.ExceptionChecker;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
 // 숫자와 구분자를 분리
 public class SeparationNum {
-    public static String[] separateNum(String str, ArrayList<String> separatorList) {
+    public static String[] separateNum(String numStr, ArrayList<String> separatorList) {
         //예외처리(맨앞이나 뒤에 구분자가 있는지)
-        ExceptionChecker.checkSeparatorPosition(str);
+        ExceptionChecker.checkSeparatorPosition(numStr);
 
         // 같은 기호가 구분자로 여러번 나오는 경우
         // 예) //< <<\n1<2<<3
         separatorList.sort(Collections.reverseOrder());
 
-        for (String s : separatorList) {
-            str = str.replace(s, " ");
+        for (String separator : separatorList) {
+            numStr = numStr.replace(separator, " ");
         }
 
-        //예외처리(구분자가 2번 연속으로 나오는지)
-        ExceptionChecker.checkContinuousSeparator(str);
+        //예외처리(구분자가 2번 연속으로 나오는 경우)
+        ExceptionChecker.checkContinuousSeparator(numStr);
 
-        return str.split(" ");
+        return numStr.split(" ");
     }
 }
