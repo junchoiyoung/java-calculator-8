6528cbb2589b51248d65f4c83c8c3986101ae23a
최준영 <cjy4780@naver.com>
2025-10-20 01:35:57 +0900

    refactor: 직관성을 위한 변수명과 메소드명 수정
    String[] num -> String[] numArr
    String s -> String num

--- src/main/java/calculator/calculation/SumNum.java
+++ src/main/java/calculator/calculation/SumNum.java
@@ -3,14 +3,14 @@ package calculator.calculation;
 import calculator.exception.ExceptionChecker;
 
 public class SumNum {
-    public static int sum(String[] num) {
+    public static int sum(String[] numArr) {
         int value = 0;
 
-        ExceptionChecker.checkInvalidSeparator(num);
-        ExceptionChecker.checkHyphen(num);
+        ExceptionChecker.checkInvalidSeparator(numArr);
+        ExceptionChecker.checkHyphen(numArr);
 
-        for (String s : num) {
-            value += Integer.parseInt(s);
+        for (String num : numArr) {
+            value += Integer.parseInt(num);
         }
 
         return value;
